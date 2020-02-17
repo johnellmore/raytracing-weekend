58d251b596e1f68b3ce4341e008d555a2f630675
John Ellmore <jnellmore@gmail.com>
2020-02-16 21:04:57 -0600

    Finish Step 8 - diffuse materials

--- main.cpp
+++ main.cpp
@@ -5,16 +5,23 @@
 #include "random.h"
 #include <iostream>
 
-inline vec3 normal_to_color(const vec3& vec) {
-    return 0.5*vec3(vec.x()+1, vec.y()+1, vec.z()+1);
+vec3 random_in_unit_sphere() {
+    vec3 p;
+    do {
+        p = 2.0*vec3(
+            random_double(),
+            random_double(),
+            random_double()
+        ) - vec3(1, 1, 1);
+    } while (p.squared_length() >= 1.0);
+    return p;
 }
 
 vec3 color(const ray& r, hittable *world) {
     hit_record rec;
-    if (world->hit(r, 0.0, MAXFLOAT, rec)) {
-        // we hit something in the world; get the color which
-        // represents the normal
-        return normal_to_color(rec.normal);
+    if (world->hit(r, 0.001, MAXFLOAT, rec)) {
+        vec3 target = rec.p + rec.normal + random_in_unit_sphere();
+        return 0.5 * color(ray(rec.p, target - rec.p), world);
     } else {
         // return the color of the world backdrop
         vec3 unit_direction = unit_vector(r.direction());
@@ -52,6 +59,10 @@ int main() {
     for (int j = ny-1; j >= 0; j--) {
         // scan from left to right (i)
         for (int i = 0; i < nx; i++) {
+
+            // enable antialiasing; send lots of rays for this
+            // pixel at slightly different offsets, then average
+            // the resulting color values
             vec3 col(0, 0, 0);
             for (int s = 0; s < ns; s++) {
                 float u = float(i + random_double()) / float(nx);
@@ -60,6 +71,9 @@ int main() {
                 col += color(r, world);
             }
             col /= float(ns);
+
+            // gamma correct the pixel
+            col = vec3(sqrt(col[0]), sqrt(col[1]), sqrt(col[2]));
 
             // convert the color to PPM int output and print
             int ir = int(255.99*col[0]);
