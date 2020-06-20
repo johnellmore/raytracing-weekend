1c8681bf0f081660f66ab8b58f6f8812e3fcdcd6
John Ellmore <jnellmore@gmail.com>
2020-06-20 11:45:56 -0500

    small tweaks

--- main.cpp
+++ main.cpp
@@ -26,8 +26,8 @@ vec3 color(const ray& r, hittable *world, int depth) {
 
 int main() {
     // the output image size
-    int nx = 200;
-    int ny = 100;
+    int nx = 1000;
+    int ny = 500;
 
     // the number of samples per pixel (antialiasing)
     int ns = 100;
@@ -40,8 +40,8 @@ int main() {
     hittable *list[4];
     list[0] = new sphere(vec3(0, 0, -1), 0.5, new lambertian(vec3(0.8, 0.3, 0.3)));
     list[1] = new sphere(vec3(0, -100.5, -1), 100, new lambertian(vec3(0.8, 0.8, 0.0)));
-    list[2] = new sphere(vec3(1, 0, -1), 0.5, new metal(vec3(0.8, 0.6, 0.2), 0.3));
-    list[3] = new sphere(vec3(-1, 0, -1), 0.5, new metal(vec3(0.8, 0.8, 0.8), 1.0));
+    list[2] = new sphere(vec3(1, 0, -1), 0.5, new metal(vec3(0.8, 0.6, 0.2), 0.0));
+    list[3] = new sphere(vec3(-1, 0, -1), 0.5, new metal(vec3(0.8, 0.8, 0.8), 0.5));
     hittable *world = new hittable_list(list, 4);
 
     // the camera that is viewing the world
